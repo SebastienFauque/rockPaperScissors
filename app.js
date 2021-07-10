189ffc3c3f11aafe72db644349f9c6c6f56fa58a
SebastienFauque <61689730+SebastienFauque@users.noreply.github.com>
2021-07-09 20:55:28 -0700

    add button push functionality

--- app.js
+++ app.js
@@ -1,1 +1,107 @@
-console.log('hello, Jeff');
+//bugs
+//user: s comp: r //doesn't update comp score, no error
+//user: s comp: s //typeError //potential solutions: remove emoji= DID NOT FIX // potential: fix id tag in html file to be computer-score on line 16
+
+
+
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector("score-board");
+const result_p = document.querySelector(".result >p");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+function getComputerChoice() {
+  const choices = ['r', 'p', 's'];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return choices[randomNumber];
+}
+
+function convertToWord(letter) {
+  if (letter === 'r') return "Rock";
+  if (letter === 'p') return "Paper";
+  if (letter === 's') return "Scissors";
+}
+
+function win(userChoice, computerChoice) {
+  const smallUserWord = "user".fontsize(3).sup();
+  const smallCompWord = "comp".fontsize(3).sup();
+  const userChoice_div = document.getElementById(userChoice);
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  result_p.innerHTML = `${convertToWord(userChoice)(user)} beats ${convertToWord(computerChoice)(comp)} you win.`;
+  userChoice_div.classList.add('green-glow');
+  setTimeout(() => userChoice_div.classList.remove('green-glow'), 333);
+}
+
+
+function lose(userChoice, computerChoice) {
+  const smallUserWord = "user".fontsize(3).sup();
+  const smallCompWord = "comp".fontsize(3).sup();
+  const userChoice_div = document.getElementById(userChoice);
+  computerScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  result_p.innerHTML = `${convertToWord(userChoice)(user)} loses to ${convertToWord(computerChoice)(comp)} you lose.`;
+  userChoice_div.classList.add('red-glow');
+  setTimeout(() => userChoice_div.classList.remove('red-glow'), 333);
+}
+
+function draw(userChoice, computerChoice) {
+  const smallUserWord = "user".fontsize(3).sup();
+  const smallCompWord = "comp".fontsize(3).sup();
+  const userChoice_div = document.getElementById(userChoice);
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  result_p.innerHTML = `${convertToWord(userChoice)(user)} draws ${convertToWord(computerChoice)(comp)} you tie.`;
+  userChoice_div.classList.add('grey-glow');
+  setTimeout(() => userChoice_div.classList.remove('grey-glow'), 333);
+}
+
+function game(userChoice) {
+  const computerChoice = getComputerChoice();
+  console.log("user choice => " + userChoice);
+  console.log("computer choice => " + computerChoice);
+  switch (userChoice + computerChoice) {
+    case "rs":
+    case "pr":
+    case "sp":
+      win(userChoice, computerChoice);
+      break;
+    case "rp":
+    case "rp":
+    case "rp":
+      lose(userChoice, computerChoice);
+      break;
+    case "ss":
+    case "rr":
+    case "pp":
+      draw(userChoice, computerChoice);
+      break;
+  }
+}
+
+
+
+function main(){
+  rock_div.addEventListener('click', function() {
+    console.log("u click rok" )
+    game("r");
+  })
+  paper_div.addEventListener('click', function() {
+    console.log("u click papr" )
+    game("p");
+  })
+  scissors_div.addEventListener('click', function() {
+    console.log("u click skis" )
+    game("s");
+  })
+}
+
+
+
+main();
